97cf4899bafbac0b3bb312bc774c41f12520eac4
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-02-09 12:47:01 +0300

    add @karma_carmody

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+  { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264', post: false },
   { username: 'akurganow', start: '01 Feb 2016', first: '694078672635064321' },
 ];
