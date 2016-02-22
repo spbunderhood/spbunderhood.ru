dc53f02a489a05da276e85ab5f8ec540c0891916
Alexander Kurganov <me@akurganow.ru>
2016-02-22 16:38:16 +0300

    Add KsuSunochki

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264', post: false },
+  { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506', post: false },
+  { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
   { username: 'akurganow', start: '01 Feb 2016', first: '694078672635064321' },
 ];
