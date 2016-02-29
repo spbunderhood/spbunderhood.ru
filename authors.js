c84c35fed495a05097cc9270826197275274c100
Alexander Kurganov <me@akurganow.ru>
2016-02-29 04:44:08 +0300

    Change karma_carmody image

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+  { username: 'shatzzz25', start: '29 Feb 2016', first: '', post: false },
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
   { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
   { username: 'akurganow', start: '01 Feb 2016', first: '694078672635064321' },
