e89e82902918885fe8acbb2b2357760f9ff99acd
Alexander Kurganov <me@akurganow.ru>
2016-03-10 03:44:54 +0300

    Publish shatzzz25

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617', post: false },
+  { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617'},
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
   { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
   { username: 'akurganow', start: '01 Feb 2016', first: '694078672635064321' },
