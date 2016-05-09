2426d7b6a2d45a19c43be5d450899fb10780251c
Alexander Kurganov <me@akurganow.ru>
2016-05-09 13:12:29 +0300

    Add juliatimoshenko

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+  { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912', post: false},
   { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617'},
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
   { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
