5ef4096cef323e2b606f34698b17f264084b90c3
Alexander Kurganov <me@akurganow.ru>
2016-05-16 13:39:35 +0300

    Add yellowsparrow_

--- authors.js
+++ authors.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912', post: false},
+  { username: 'yellowsparrow_', start: '16 May 2016', first: '732154549021081600', post: false},
+  { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912'},
   { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617'},
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
   { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
