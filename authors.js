89c40cb455ea11f77d32ea447fda80409f5f7bac
Alexander Kurganov <me@akurganow.ru>
2016-05-23 10:45:21 +0300

    Add EcTsUj2JDpMy2Qn

--- authors.js
+++ authors.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'yellowsparrow_', start: '16 May 2016', first: '732154549021081600', post: false},
+  { username: 'EcTsUj2JDpMy2Qn', start: '23 May 2016', first: '734650327648112640', post: false},
+  { username: 'yellowsparrow_', start: '16 May 2016', first: '732154549021081600'},
   { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912'},
   { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617'},
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
