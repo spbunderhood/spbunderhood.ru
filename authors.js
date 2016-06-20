dc0c5257720739397c1f64662c24237d278fb991
Alexander Kurganov <me@akurganow.ru>
2016-06-20 11:43:55 +0300

    Add maria_lobareva

--- authors.js
+++ authors.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'maria_lobareva', start: '20 June 2016', first: '744807470346887168', post: false},
-  { username: 'ptica_elsze', start: '13 June 2016', first: '742305153747947520'},
-  { username: 'TheatreSubbota', start: '06 June 2016', first: '739733925925388288'},
-  { username: '123PorRosa', start: '30 May 2016', first: '737226514014588928'},
-  { username: 'EcTsUj2JDpMy2Qn', start: '23 May 2016', first: '734650327648112640'},
-  { username: 'yellowsparrow_', start: '16 May 2016', first: '732154549021081600'},
-  { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912'},
-  { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617'},
+  { username: 'maria_lobareva', start: '20 June 2016', first: '744807470346887168', post: false },
+  { username: 'ptica_elsze', start: '13 June 2016', first: '742305153747947520' },
+  { username: 'TheatreSubbota', start: '06 June 2016', first: '739733925925388288' },
+  { username: '123PorRosa', start: '30 May 2016', first: '737226514014588928' },
+  { username: 'EcTsUj2JDpMy2Qn', start: '23 May 2016', first: '734650327648112640' },
+  { username: 'yellowsparrow_', start: '16 May 2016', first: '732154549021081600' },
+  { username: 'juliatimoshenko', start: '9 May 2016', first: '729612584152870912' },
+  { username: 'shatzzz25', start: '29 Feb 2016', first: '704213348804591617' },
   { username: 'ksusunochki', start: '22 Feb 2016', first: '701537979060330506' },
-  { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264'},
+  { username: 'karma_carmody', start: '08 Feb 2016', first: '696667537963147264' },
   { username: 'akurganow', start: '01 Feb 2016', first: '694078672635064321' },
 ];
