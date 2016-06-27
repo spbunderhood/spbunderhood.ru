3d6590585e7c741b418315a96a24a05a84817878
Alexander Kurganov <me@akurganow.ru>
2016-06-27 10:24:28 +0300

    Update authors files

--- authors.js
+++ authors.js
@@ -8,7 +8,7 @@ export default [
   { username: '123PorRosa',      start: '30 May 2016',  first: '737226514014588928' },
   { username: 'EcTsUj2JDpMy2Qn', start: '23 May 2016',  first: '734650327648112640' },
   { username: 'yellowsparrow_',  start: '16 May 2016',  first: '732154549021081600' },
-  { username: 'juliatimoshenko', start: '9 May 2016',   first: '729612584152870912' },
+  { username: 'juliatimoshenko', start: '09 May 2016',  first: '729612584152870912' },
   { username: 'shatzzz25',       start: '29 Feb 2016',  first: '704213348804591617' },
   { username: 'ksusunochki',     start: '22 Feb 2016',  first: '701537979060330506' },
   { username: 'karma_carmody',   start: '08 Feb 2016',  first: '696667537963147264' },
