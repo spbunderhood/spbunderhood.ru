2d4d75916100dc218c3857449af29ff9768fc484
Realetive <realetive@yandex.ru>
2016-08-06 21:59:20 +0300

    Add missed makatya

--- authors.js
+++ authors.js
@@ -2,6 +2,7 @@
 
 export default [
   { username: 'Lightinsite',     start: '01 Aug 2016',  first: '760120678356021248', post: false },
+  { username: 'makatya',         start: '25 July 2016', first: '757513341283753984' },
   { username: 'alena_aleshina',  start: '18 July 2016', first: '754952127093542912' },
   { username: 'AntZoo',          start: '11 July 2016', first: '752379864964014080' },
   { username: 'turclub_pik',     start: '04 July 2016', first: '749901532154499072' },
